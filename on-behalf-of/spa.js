5e37939ab5f6307204883f6415193077938cd978
Chao Chen <cchen@artisreit.com>
2022-04-18 16:53:25 -0500

    save work

--- on-behalf-of/spa.js
+++ on-behalf-of/spa.js
@@ -7,8 +7,12 @@ const DEFAULT_PORT = process.env.PORT || 3000;
 const app = express();
 
 // Set up a route for index.html
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/spa.html'));
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname + '/spa_test.html'));
 });
 
 app.listen(DEFAULT_PORT, () => {
